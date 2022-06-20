7100d5f12920ded181f974687d5fab4a6770c31d
Richie McIlroy <richie@mcilroy.co>
2022-06-20 19:54:11 +0100

    updates: LP copywriting, navbar onClick

--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -71,9 +71,9 @@ export default function Navbar() {
             {
               active &&
               <div className="origin-top-right absolute left-0 top-auto overflow-hidden mt-12 w-full shadow-xl border-t-4 border-gray-200 bg-white z-50">
-                <a className="block p-5 text-md bg:text-white hover:bg-gray-100 border-b-2 border-gray-200" href="/#why-reflio">Why Reflio</a>
-                <a className="block p-5 text-md bg:text-white hover:bg-gray-100 border-b-2 border-gray-200" href="/#pricing">Pricing</a>
-                <a className="block p-5 text-md bg:text-white hover:bg-gray-100 font-semibold" href={user ? '/dashboard' : '/#intro'}>{user ? 'Dashboard' : 'Join Waitlist' }</a>
+                <a onClick={e=>{setActive(false)}} className="block p-5 text-md bg:text-white hover:bg-gray-100 border-b-2 border-gray-200" href="/#why-reflio">Why Reflio</a>
+                <a onClick={e=>{setActive(false)}} className="block p-5 text-md bg:text-white hover:bg-gray-100 border-b-2 border-gray-200" href="/#pricing">Pricing</a>
+                <a onClick={e=>{setActive(false)}} className="block p-5 text-md bg:text-white hover:bg-gray-100 font-semibold" href={user ? '/dashboard' : '/#intro'}>{user ? 'Dashboard' : 'Join Waitlist' }</a>
               </div>
             }
 
