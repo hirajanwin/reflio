063e0eb4b481ca050edbf10c6e53cc9fa1d9765e
Richie McIlroy <richie@mcilroy.co>
2022-06-20 19:55:57 +0100

    fix: Modal positioning

--- components/ui/Modal.js
+++ components/ui/Modal.js
@@ -8,7 +8,7 @@ const Modal = (props) => {
   return(
     <Transition.Root show={props?.modalOpen} as={Fragment}>
       <Dialog as="div" className="fixed z-50 inset-0 overflow-y-auto" onClose={props?.setModalOpen}>
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
